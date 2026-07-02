f985e907a1e155a44ad3fe4563d8d58b19f39ac2
joscelynchua <25003773@myrp.edu.sg>
2026-07-02 15:50:49 +0800

    first commit

---
+++ StudentListApp/app.js
@@ -0,0 +1,83 @@
+const express = require('express'); 
+const mysql = require('mysql2'); 
+const app = express(); 
+ 
+// Create MySQL connection 
+const connection = mysql.createConnection({     
+    host: 'localhost',     
+    user: 'root',     
+    password: 'RP738964$',     
+    database: 'c237_studentlistapp' 
+}); 
+ 
+connection.connect((err) => 
+{   if (err) { 
+    console.error('Error connecting to MySQL:', err);
+return; 
+    } 
+    console.log('Connected to MySQL database'); 
+}); 
+ 
+// Set up view engine 
+app.set('view engine', 'ejs'); 
+//  enable static files 
+app.use(express.static('public')); 
+app.use(express.urlencoded({ extended: false }));
+// Define routes
+app.get('/', (req, res) => {
+    const sql = 'SELECT * FROM student';
+  // Fetch data from MySQL
+    connection.query( sql , (error, results) => {
+        if (error) {
+            console.error('Database query error:', error.message);
+            return res.send('Error Retrieving students'); 
+        }
+        // Render HTML page with data
+        res.render('index', { student: results });
+    });
+});
+
+app.get('/student/:id', (req, res) => {
+  // Extract the student ID from the request parameters   
+    const student_id = req.params.id;
+    const sql = 'SELECT * FROM student WHERE id = ?';
+  // Fetch data from MySQL based on the student ID   
+    connection.query( sql , [student_id], (error, results) => {
+        if (error) {
+            console.error('Database query error:', error.message);       
+            return res.send('Error Retrieving student by ID'); 
+    }
+    // Check if any student with the given ID was found
+        if (results.length > 0) {
+      // Render HTML page with the student data
+            res.render('student', { student: results[0] });
+        } else {
+      // If no student with the given ID was found
+            res.send('Student not found');
+        }
+    });
+});
+
+app.get('/addStudent', (req, res) => {
+  res.render('addStudent'); 
+});
+
+app.post('/addStudent', (req, res) => {
+  // Extract student data from the request body   
+    const { name, contact, dob, image } = req.body;   
+    const sql = 'INSERT INTO student (name, contact, dob, image) VALUES (?, ?, ?, ?)';
+  // Insert the new student into the database
+    connection.query( sql , [name, contact, dob, image], (error, results) => {     
+        if (error) {
+      // Handle any error that occurs during the database operation       
+            console.error("Error adding student:", error);       
+            res.send('Error adding student');
+        } else {
+      // Send a success response       
+        res.redirect('/');
+        }
+  }); 
+});
+
+const PORT = process.env.PORT || 3000; 
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
